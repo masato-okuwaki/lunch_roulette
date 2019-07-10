c5005f03f1126fc4c2e579f6776e5ef6c75574c9
suguru_katsuyama <zgok105@yahoo.co.jp>
2019-07-10 20:22:52 +0900

    remove-roulette-model

--- test/controllers/roulette_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RouletteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get roulette_index_url
-    assert_response :success
-  end
-
-end
